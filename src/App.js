31de6be244d5028a68c2e6dcb38876f8aa2efff0
whollahan <bakuhf@gmail.com>
2022-08-31 17:46:52 -0500

    edited readme file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [calculator, setCalculator] = useState("")
+  const [result, setResult] = useState("")
+  const signs = ['+', '-', '*', '/', '.']
+
+  const inputToCalculator = (input) => {
+      if (signs.includes(input) && calculator === '' || 
+          signs.includes(input) && signs.includes(calculator.slice(-1))) {
+        return;
+      }
+      setCalculator(calculator + input)
+
+      if (!signs.includes(input)) {
+        setResult(eval(calculator + input).toString())
+      }
+  }
+
+  const runCalculator = () => {
+    setCalculator(eval(calculator).toString())
+  }
+
+  const deleteButton = () => {
+    if (calculator == '') {
+      return;
+    } else {
+      const val = calculator.replace(calculator, "")
+      setCalculator(val)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="calc">
+          <div className="westernlogo">
+            Western Express
+          </div>
+          <div className="calcdisplay">
+              { calculator || "0" }
+          </div>
+
+          <div className="mathSigns">
+              <button onClick={() => inputToCalculator('+')}>+</button>
+              <button onClick={() => inputToCalculator('-')}>-</button>
+              <button onClick={() => inputToCalculator('*')}>*</button>
+              <button onClick={() => inputToCalculator('/')}>/</button>
+              <button onClick={() => deleteButton()}>C</button>
+          </div>
+
+          <div className="numbers">
+              <button onClick={() => inputToCalculator('1')}>1</button>
+              <button onClick={() => inputToCalculator('2')}>2</button>
+              <button onClick={() => inputToCalculator('3')}>3</button>
+              <button onClick={() => inputToCalculator('4')}>4</button>
+              <button onClick={() => inputToCalculator('5')}>5</button>
+              <button onClick={() => inputToCalculator('6')}>6</button>
+              <button onClick={() => inputToCalculator('7')}>7</button>
+              <button onClick={() => inputToCalculator('8')}>8</button>
+              <button onClick={() => inputToCalculator('9')}>9</button>
+              <button onClick={() => inputToCalculator('.')}>.</button>
+              <button onClick={() => inputToCalculator('0')}>0</button>
+              <button onClick={() => runCalculator()}>=</button>
+          </div>
+        </div>
     </div>
   );
 }
